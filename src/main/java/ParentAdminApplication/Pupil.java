7eade94c17eb6d3a2c3296168ffe023de6612244
ellankusi <nkusiella5@gmail.com>
2025-04-08 23:25:34 +0100

    getDob

--- src/main/java/ParentAdminApplication/Pupil.java
+++ src/main/java/ParentAdminApplication/Pupil.java
@@ -14,7 +14,9 @@ public class Pupil {
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
-    public LocalDate getDob(){
-        return dob;
-    }
+    public LocalDate getDob() { return dob; }
+    public void setDob(LocalDate dob) { this.dob = dob; }
+
+    public String getCohortId() { return cohortId; }
+    public void setCohortId(String cohortId) {this.cohortId = cohortId; }
 }
