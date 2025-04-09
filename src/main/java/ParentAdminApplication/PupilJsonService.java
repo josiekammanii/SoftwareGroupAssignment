ca90a1dd126b15584f9631113c0e440e449afd7a
ellankusi <nkusiella5@gmail.com>
2025-04-09 09:26:35 +0100

    importing jackson

--- src/main/java/ParentAdminApplication/PupilJsonService.java
+++ src/main/java/ParentAdminApplication/PupilJsonService.java
@@ -2,6 +2,8 @@ package ParentAdminApplication;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
