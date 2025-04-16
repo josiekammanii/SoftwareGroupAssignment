38bf3da34140c0c99566b502c34ac7644387a2a0
Joelle <joellesteiner2005@gmail.com>
2025-04-16 13:57:19 +0100

    enabled events to be added

--- src/main/java/ParentAdminApplication/AdminController.java
+++ src/main/java/ParentAdminApplication/AdminController.java
@@ -1,19 +1,28 @@
 package ParentAdminApplication;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/api")
 public class AdminController {
+
+    private final JsonService jsonService;
+
+    @Autowired
+    public AdminController(JsonService jsonService) {
+        this.jsonService = jsonService;
+    }
 
     @PostMapping("/events")
     public ResponseEntity<String> createEvent(@RequestBody Event event) {
         try {
-            JsonService.saveEvent(event);
+            jsonService.saveEvent(event);
             return ResponseEntity.ok("Event saved successfully");
         } catch (Exception e) {
             return ResponseEntity.status(500).body("Error saving event");
         }
     }
-
 }
+
