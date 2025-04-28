8443ced2e3444e6d8264b192323cbb24380975c8
ellankusi <nkusiella5@gmail.com>
2025-04-29 00:56:57 +0100

    Revert "removing mongo"
    This reverts commit 4784b7230108a5a891365a42cd3c3078d76b5a06.

--- src/main/java/ParentAdminApplication/EventController.java
+++ src/main/java/ParentAdminApplication/EventController.java
@@ -1,5 +1,5 @@
 package ParentAdminApplication;
-import ParentAdminApplication.Event;
+import jdk.jfr.Event;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
